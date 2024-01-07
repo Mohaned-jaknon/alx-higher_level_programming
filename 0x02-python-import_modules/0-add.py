4e5d8c7bb559e31b8bbaaa318deba399f5a222a3
Mohaned_jaknon <mohanedjaknon@gmail.com>
2024-01-07 15:58:28 -0800

    done2

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-# Assign the values to variables a and b
+# Assign the value 1 to variable a
 a = 1
+
+# Assign the value 2 to variable b
 b = 2
 
-# Define the add function
-exec(open('add_0.py').read())
+# Define the add function using the content of add_0.py
+add_code = """
+def add(a, b):
+    \"\"\"My addition function
 
-# Print the result of the addition
-print('{} + {} = {}'.format(a, b, add(a, b)))
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    \"\"\"
+    return (a + b)
+"""
+add = {}
+exec(add_code, add)
+
+# Print the result of the addition using string formatting
+print('{} + {} = {}'.format(a, b, add['add'](a, b)))
