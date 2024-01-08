4b3de0b36581a3811faebbd531f7160bc7c41418
Mohaned_jaknon <mohanedjaknon@gmail.com>
2024-01-07 16:24:55 -0800

    done3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
-# Assign the value 1 to variable a
-a = 1
-
-# Assign the value 2 to variable b
-b = 2
-
-# Define the add function using the content of add_0.py
-add_code = """
-def add(a, b):
-    \"\"\"My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    \"\"\"
-    return (a + b)
-"""
-add = {}
-exec(add_code, add)
-
-# Print the result of the addition using string formatting
-print('{} + {} = {}'.format(a, b, add['add'](a, b)))
+if __namme__ == "___main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{} + {] = {}".format(a, b, add(a, b)))
